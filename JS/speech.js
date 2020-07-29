552486f269f0bcfb902c46ff06e4998a43bc4e71
syed haider imam abidi <abidihaider89@gmail.com>
2020-07-30 00:07:12 +0500

    updatesMadeToTheProject

--- JS/speech.js
+++ JS/speech.js
@@ -1,13 +1,16 @@
 const Q = document.querySelector('.questionDiv p');
 const A = document.querySelector('.answerDiv p');
 const talkButton = document.querySelector('.buttonDiv button');
+const previousActivity = document.querySelector('.previousActivity');
+const history = document.querySelector('.history');
 const speechObject = window.webkitSpeechRecognition;
 const speech = new speechObject();
 let recognitionInProgress = false;
 
 window.onload = () => {
-    Q.style.display='none',
-    A.style.display='none'
+    Q.style.display='none';
+    A.style.display='none';
+    history.style.display='none';
 }
 
 const replies = [
@@ -36,15 +39,23 @@ talkButton.addEventListener('click',()=>{
 });
 
 speech.addEventListener('result',(e)=>{
+    history.style.display='block';
     recognitionInProgress=false;
     Q.textContent = e.results[0][0].transcript;
+    addInHistory(Q.textContent);
     Q.style.display = "block";
     reply();
 })
 
+function addInHistory(Text){
+    const p = document.createElement('p');
+    p.textContent = Text;
+    previousActivity.appendChild(p);
+}
+
 function reply(){
     const randomReply = new SpeechSynthesisUtterance();
-    randomReply.text = replies[Math.floor(Math.random()*replies.length)+1];
+    randomReply.text = replies[Math.floor(Math.random()*replies.length)];
     A.textContent = (randomReply.text);
     A.style.display = "block";
     window.speechSynthesis.speak(randomReply);
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #speech Recognition and Reply Back
 
-This is a simple app in which the user says some thing which can be seen on the screen and the user can see and hear a random reply
+This is a simple app in which the user says some thing which can be seen on the screen and the user can see and hear a random reply. Also this app also includes a div showing all the previous chat history
